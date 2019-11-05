e35f1cb9826b408a8899e31db673f56f863a10a0
= <=>
2019-11-06 01:00:35 +0400

    Add Cross origin

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -30,6 +30,17 @@ module GTUStore
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options]
+          )
+      end
+    end
+
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
   end
 end

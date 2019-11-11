f9a5e9bb1601790ff9a04726064d63f4930518a8
robertguitar20 <roborobert16@gmail.com>
2019-11-12 00:24:15 +0400

    {{}}

--- frontend/gtuShop/node_modules/@ng-bootstrap/ng-bootstrap/dropdown/dropdown.module.d.ts
+++
@@ -1,4 +0,0 @@
-export { NgbDropdown, NgbDropdownAnchor, NgbDropdownToggle, NgbDropdownMenu, NgbDropdownItem } from './dropdown';
-export { NgbDropdownConfig } from './dropdown-config';
-export declare class NgbDropdownModule {
-}
